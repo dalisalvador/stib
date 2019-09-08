9aa90924dfd3cea460d4b7aca877da49e8326251
F <f@MBP-de-F.lan>
2019-09-08 10:16:31 +0200

    delete vehiclu bug fixed. When line was deleted, vehicles from myLines were still shown

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ import map from './assets/map';
 import allStops from './assets/info/stops';
 import lines from './assets/info/lines';
 import MapBox from './Components/MapBox/MapBox';
-import Toast, {DURATION} from 'react-native-easy-toast'
+import Toast, {DURATION} from 'react-native-easy-toast';
 
 const App = () => {
   const [marginBottom, setMarginBottom] = useState(1);
@@ -45,15 +45,21 @@ const App = () => {
     type: 'FeatureCollection',
     features: [],
   });
+
   const [vehiculesGeoJson, setVehiculesGeoJson] = useState({
     type: 'FeatureCollection',
     features: [],
   });
 
-  const toast = useRef(null)
+  const [myVehiculesGeoJson, setMyVehiculesGeoJson] = useState({
+    type: 'FeatureCollection',
+    features: [],
+  });
+
+  const toast = useRef(null);
   //Settings
-  const [allVehicles, setAllVehicles] = useState(false)
-  const [showStopName, setShowStopName] = useState(false)
+  const [allVehicles, setAllVehicles] = useState(false);
+  const [showStopName, setShowStopName] = useState(false);
 
   useEffect(() => {
     setAllLines(map.initLines(allStops));
@@ -61,51 +67,59 @@ const App = () => {
 
   useEffect(() => {
     updateAllVehicleGeoJson(allLinesRef.current, myLinesRef.current);
-    setInterval(() => {updateAllVehicleGeoJson(allLinesRef.current, myLinesRef.current)}, 1000*35)
+    setInterval(() => {
+      updateAllVehicleGeoJson(allLinesRef.current, myLinesRef.current);
+    }, 1000 * 35);
   }, [allLines]);
 
-
   const addLine = selectedLine => {
-    if(!myLines.find(line => line.selection.nroStop === selectedLine.nroStop &&  line.selection.variantStop === selectedLine.variantStop )) {
-    map.addToMyLines(selectedLine).then(response => {
-      setMyLines([...myLines, response.newLine]);
-      addLineToGeoJson(response.newLine);
-      editVehiclesGeoJson(selectedLine, "add");
-    });
-    toast.current.show("line Added", 2000)
-    } else toast.current.show("Line already stored in 'myLines'", 2000)
+    if (
+      !myLines.find(
+        line =>
+          line.selection.nroStop === selectedLine.nroStop &&
+          line.selection.variantStop === selectedLine.variantStop,
+      )
+    ) {
+      map.addToMyLines(selectedLine).then(response => {
+        setMyLines([...myLines, response.newLine]);
+        addLineToGeoJson(response.newLine);
+        editVehiclesGeoJson(selectedLine, 'add');
+      });
+      toast.current.show('line Added', 2000);
+    } else toast.current.show("Line already stored in 'myLines'", 2000);
   };
 
-  const editVehiclesGeoJson = (line, action) =>{
+  const editVehiclesGeoJson = (line, action) => {
     let newFeatures = vehiculesGeoJson.features;
-    if(action === "add"){
-        vehiculesGeoJson.features.map((vehicle,i) => {
-        if(vehicle.properties.numero_lig === line.nroStop &&
-        vehicle.properties.variante === line.variantStop) {
+    if (action === 'add') {
+      vehiculesGeoJson.features.map((vehicle, i) => {
+        if (
+          vehicle.properties.numero_lig === line.nroStop &&
+          vehicle.properties.variante === line.variantStop
+        ) {
           newFeatures[i].properties.myLine = 1;
         }
-      })
-
-    }
-    else {
-      vehiculesGeoJson.features.map((vehicle,i) => {
-      if(vehicle.properties.numero_lig === line.nroStop &&
-      vehicle.properties.variante === line.variantStop) {
-        console.log(i)
-        newFeatures[i].properties.myLine = 0;
+      });
+    } else {
+      vehiculesGeoJson.features.map((vehicle, i) => {
+        if (
+          vehicle.properties.numero_lig === line.selection.nroStop &&
+          vehicle.properties.variante === line.selection.variantStop
+        ) {
+          newFeatures[i].properties.myLine = 0;
         }
-      })
-    }        
+      });
+    }
 
     setVehiculesGeoJson({
       ...vehiculesGeoJson,
-      features: newFeatures
-    })
-  }
+      features: newFeatures,
+    });
+  };
 
   const deleteLine = lineTodelete => {
     deleteLineFromGeoJson(lineTodelete);
-    editVehiclesGeoJson(lineTodelete, "delete");
+    editVehiclesGeoJson(lineTodelete, 'delete');
     setMyLines([
       ...myLines.filter((line, i) => myLines.indexOf(lineTodelete) !== i),
     ]);
@@ -122,82 +136,84 @@ const App = () => {
     });
   };
 
-    const addVehiculesToGeoJson = features => {
+  const addVehiculesToGeoJson = features => {
     setVehiculesGeoJson({
       ...vehiculesGeoJson,
       features,
     });
   };
 
-  const deleteStopsFromGeoJson = (lineTodelete) => {
+  const deleteStopsFromGeoJson = lineTodelete => {
     setGeoJson({
-        ...geoJson,
-        features: [
-          ...geoJson.features.filter(
-            (feature, i) =>
-              geoJson.features.indexOf(line.line.shape.features[0]) !== i
-          ),
-        ],
-      });
-  }
-
-  const updateAllVehicleGeoJson = async (allLines, myLines) => {
-    if(allLines){
-      let responses = await map.updateAllVehicles(chunkArray(allLines.map(line => line.nroStop).filter((x,i,arr) =>arr.indexOf(x)=== i),10));
-
-      Promise.all(responses).then(features => 
-        
-        {
-          //add "myLine=1" property if vahicule exists in myLines
-          let concatenatedFeatures = features.reduce((a,b)=>{
-              return a.concat(b)
-          })
-          concatenatedFeatures.map((vehicle,i) => {
-            myLines.map(line => {
-                if(vehicle.properties.numero_lig === line.selection.nroStop &&
-                   vehicle.properties.variante === line.selection.variantStop &&
-                    vehicle.properties.mode === line.selection.mode[0]
-                 )
-                 {
-                   concatenatedFeatures[i].properties.myLine = 1
-                 }
-                 
-            })
-          })
-
-          setVehiculesGeoJson({
-            ...vehiculesGeoJson,
-            features: concatenatedFeatures
-          })  
-          }
-        )
-    }
-   }
-  
-  const chunkArray = (myArray, chunk_size) => {
-    var results = [];
-    
-    while (myArray.length) {
-        results.push(myArray.splice(0, chunk_size));
-    }
-    
-    return results;
-}
-
-
-
-  const deleteLineFromGeoJson = line => {
-  let newfeatures = geoJson.features.filter(feature => line.line.stops.indexOf(feature) ===-1)
-  newfeatures = newfeatures.filter(feature => line.line.shape.features.indexOf(feature) === -1)
-
-  setGeoJson({
-        ...geoJson,
-        features: [
-          ...newfeatures,
-        ],
-      });
+      ...geoJson,
+      features: [
+        ...geoJson.features.filter(
+          (feature, i) =>
+            geoJson.features.indexOf(line.line.shape.features[0]) !== i,
+        ),
+      ],
+    });
   };
 
+  const updateAllVehicleGeoJson = async (allLines, myLines) => {
+    if (allLines) {
+      let responses = await map.updateAllVehicles(
+        chunkArray(
+          allLines
+            .map(line => line.nroStop)
+            .filter((x, i, arr) => arr.indexOf(x) === i),
+          10,
+        ),
+      );
+
+      Promise.all(responses).then(features => {
+        //add "myLine=1" property if vahicule exists in myLines
+        let concatenatedFeatures = features.reduce((a, b) => {
+          return a.concat(b);
+        });
+        concatenatedFeatures.map((vehicle, i) => {
+          myLines.map(line => {
+            if (
+              vehicle.properties.numero_lig === line.selection.nroStop &&
+              vehicle.properties.variante === line.selection.variantStop &&
+              vehicle.properties.mode === line.selection.mode[0]
+            ) {
+              concatenatedFeatures[i].properties.myLine = 1;
+            }
+          });
+        });
+
+        setVehiculesGeoJson({
+          ...vehiculesGeoJson,
+          features: concatenatedFeatures,
+        });
+      });
+    }
+  };
+
+  const chunkArray = (myArray, chunk_size) => {
+    var results = [];
+
+    while (myArray.length) {
+      results.push(myArray.splice(0, chunk_size));
+    }
+
+    return results;
+  };
+
+  const deleteLineFromGeoJson = line => {
+    let newfeatures = geoJson.features.filter(
+      feature => line.line.stops.indexOf(feature) === -1,
+    );
+    newfeatures = newfeatures.filter(
+      feature => line.line.shape.features.indexOf(feature) === -1,
+    );
+
+    setGeoJson({
+      ...geoJson,
+      features: [...newfeatures],
+    });
+  };
 
   const actions = [
     {
@@ -223,19 +239,35 @@ const App = () => {
       },
       name: 'about',
       position: 2,
-    }
+    },
   ];
 
-console.log(vehiculesGeoJson)
+  console.log(vehiculesGeoJson);
   return (
-    <AllLinesProvider value={{allLines, myLines, deleteLine, addLine, allVehicles, setAllVehicles, showStopName, setShowStopName}}>
+    <AllLinesProvider
+      value={{
+        allLines,
+        myLines,
+        deleteLine,
+        addLine,
+        allVehicles,
+        setAllVehicles,
+        showStopName,
+        setShowStopName,
+      }}>
       <Fragment>
         <View
           style={{
             width: '100%',
             height: '100%',
           }}>
-          <MapBox myLines={myLines} geoJson={geoJson} vehiculesGeoJson={vehiculesGeoJson} allStops={allStops} mapFunctions={map} />
+          <MapBox
+            myLines={myLines}
+            geoJson={geoJson}
+            vehiculesGeoJson={vehiculesGeoJson}
+            allStops={allStops}
+            mapFunctions={map}
+          />
         </View>
         <FloatingAction
           actions={actions}
@@ -246,14 +278,17 @@ console.log(vehiculesGeoJson)
             if (name === 'addLine') {
               setMainPressed(false);
               setModalVisible(true);
-            } else if(name === 'settings'){
+            } else if (name === 'settings') {
               setModalSettings(true);
             }
           }}
         />
         <ModalAdd visible={modalVisible} setModalVisible={setModalVisible} />
-        <ModalSettings modalSettings={modalSettings} setModalSettings={setModalSettings} />
-        <Toast position='bottom' ref={toast}/>
+        <ModalSettings
+          modalSettings={modalSettings}
+          setModalSettings={setModalSettings}
+        />
+        <Toast position="bottom" ref={toast} />
       </Fragment>
     </AllLinesProvider>
   );
